70cb98fe52bb845c95123728f5785d166ceebf71
treeofmapple <samuelsantana778@gmail.com>
2025-04-09 17:35:10 -0300

    base-example

---
+++ Base-Example/src/test/java/com/tom/sample/base/BaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tom.sample.base;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BaseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
